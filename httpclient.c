20fa2882ead443a2d3bd0789426d7b2276f374f8
Gustavo <tavog14@gmail.com>
2017-03-28 15:05:28 -0600

    stress junto a httpclient.c modificado

--- httpclient.c
+++ httpclient.c
@@ -8,12 +8,14 @@ typedef enum {FALSE, TRUE} Bool;
 /* 
  * funcion que llama a curl con el url pasado como argurgumento.
  */
-int curl_llamada( char *url){
+int curl_llamada( char *url, char *output){
 	printf("URL utilizado: %s\n", url);
 	char comando[400];			//tama;o del comando
 	strcpy(comando, "curl ");	
 	strcat(comando, url );
-	strcat(comando, " > resultado.html" ); // al final quedaria: curl [url] > resultado.html
+	strcat(comando, " > resultados/" ); 
+	strcat(comando, output); // al final quedaria: curl [url] > <output_name>.html
+	strcat(comando, ".html");
 	printf("%s\n", comando); 
 	system(comando);
 	return 0;
@@ -22,18 +24,19 @@ int curl_llamada( char *url){
 
 
 int main( int argc, char * argv [] ) {
-	printf( "argc = %d\n", argc );
-	
-	if (argc == 3){
+	if (argc == 5){
 		if (!(strcmp(argv[1],"-u") == 0)){
 			printf ("Parametro -u extraviado\n");
 			return 0;
-		} 
-		curl_llamada (argv[2]);  //llamada a la funcion principal
-		} else {
-			printf ("Muchos/Pocos parametros\n");
-			printf ("Sintaxis correcta es: ./httpclient -u <url>\n");
-
 		}
+		if (!(strcmp(argv[3],"-o") == 0)){
+			printf ("Parametro -o extraviado\n");
+			return 0;
+		}
+		curl_llamada (argv[2], argv[4]);  //llamada a la funcion principal
+	} else {
+		printf ("Muchos/Pocos parametros\n");
+		printf ("Sintaxis correcta es: ./httpclient -u <url>\n");
+	}
 	return 0;
 }
---
+++ stressCMD.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import threading
+from subprocess import call
+import argparse
+
+## funcion que ejecutaran los hilos
+def worker( *comando):
+	call(comando) ### comando = ["./httpclient", "-u", <url>, "-o" <output_name>"]
+	return
+
+## manejo de argumentos vio argparse library
+def manager_de_argumentos():
+	parser = argparse.ArgumentParser(description='Stress de Httpclient')
+	parser.add_argument('-n', action='store', dest='cantidad_de_hilos', help='Cantidad de hilos que se utilizaran', type=int, required=True)
+	parser.add_argument('comando', metavar='Comando_a_usar', type=str, nargs=2, help='Commando a ejecutar y sus parametros')
+	lista_comandos = parser.parse_args()
+	return lista_comandos 
+
+## funcion generadora de hilos, cada hilo crea un archivo con el id del hilo
+def generar_hilos( cantidad_hilos, comando ):
+	salida = comando[4]
+	threads = []
+	for i in range( cantidad_hilos ):
+		comando[4] = str(i)+salida
+		t = threading.Thread( target=worker , args = (comando) )
+		threads.append(t)
+		t.start()
+
+def main():
+	lista_comandos = manager_de_argumentos()
+	generar_hilos (lista_comandos.cantidad_hilos , [lista_comandos.comando[0]] + lista_comandos.comando[-1].split() )
+	
+if __name__ == "__main__":
+	main()
